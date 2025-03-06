292a60d563fb5498a17df41b2aa180674e0a3690
misha-sikandar <mishadehani@gmail.com>
2025-03-07 00:17:28 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+st.set_page_config(page_title="growth mindset project",project_icon="★")
+st.title("Growth Mindset Challenge:Web App with Streamlit")
+st.header("🚀 Welcome to Your Growth Journey!")
+st.write("Embrace challenges,learn from mistakes,and unlock your full potential. This AI powered app helps you build a growth mindset with reflection, challenges, and achievements!🌟")
+st.header("Todays Growth Mindset Quote")
+st.write("Success is not final, failure is not fatal:it is the courage to continue that counts.- Winston Churchill")
+st.header("What's your challenge today?")
+user_input=st.text_input("Describe a challenge you're facing:")
+
+if user_input:
+    st.success(f"You're facing:{user_input}.Keep pushing forward towards your goal!")
+else:
+    st.warning("Tell us about your challenge to get started!")
+
+st.header("Reflect on your learning")
+reflection=st.text_area("Write your reflection here:")
+
+if reflection:
+     st.success(f"Greate Insight! Your reflection:{reflection}")
+else:
+    st.info("Reflecting on past experience help you grow! Share your difficulties")
+
